1850bcde727c7b1536592d3705681916717b9e17
Aditya Nair <aditya.metal01@gmail.com>
2021-02-08 21:02:15 +0530

    patient report upload/download functions

--- backend/routes/catalog.js
+++ backend/routes/catalog.js
@@ -20,4 +20,60 @@ router.put('/patients/:id/update', patDtsCont.patDetsUpd);
 // DELETE request to delete the specific patient details
 router.delete('/patients/:id/delete', patDtsCont.patDetsDel);
 
+router.post('/doctors/uploadReport', upload.single('report'), async(req, res) => {
+    try {
+    const report = req.file;
+    const reportpath = req.file.location;
+    //const patient = req.body.userId
+
+    const newReport = new uplReport({report, reportpath});
+
+    const savedReport = await newReport.save();
+                // req.flash("success", "PDF successfully uploaded");
+                res.json({message: 'success', report: savedReport, reportpath: reportpath})
+                //res.render("displayPage",  {savedReport: report, path: reportpath});
+    }catch(err){
+        res.json({status: 'Upload failed', err: err.message})
+    } 
+});
+
+// GET request to download the patient report
+router.get('/doctors/reports/download/:dlKey', function(req, res, next){
+    // download the file via aws s3 here
+    const dlkey = req.params.dlKey;
+
+    //console.log('Trying to download file', fileKey);
+    //var AWS = require('aws-sdk');
+    AWS.config.update(
+      {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_ACCESS_SECRET,
+        region: 'ap-south-1'
+      }
+    );
+    var s3 = new AWS.S3();
+    const params = {
+        Bucket : process.env.BUCKET_NAME,
+        Key    : dlkey
+    }
+
+    uplReport.findOne({'report.key': dlkey})
+    .then(() => 
+    {
+    /*    
+    res.attachment(dlkey)
+    var fileStream = s3.getObject(options).createReadStream();
+    console.log(fileStream);
+    const response = JSON.stringify(JSON.parse(fileStream.pipe(res)));
+    res.json({message: 'success', result: data, report: response})
+    */
+   
+   const readStream = s3.getObject(params).createReadStream();
+   const writeStream = fs.createWriteStream(path.join(__dirname, '../report_download/'+ dlkey));
+   readStream.pipe(writeStream);
+   res.json({message: 'success'})
+}).catch(err => res.json({err: err.message}));
+    
+});
+
 module.exports = router;
