33ae44183ea7147fe2c140ecd4514120dea1bf5d
Anudeep <anudeepmarlapalli22@gmail.com>
2021-02-15 16:56:49 +0530

    Showing prescription in patient and organisation dashboard

--- frontend/src/Dashboards/OrgPrescription.js
+++ frontend/src/Dashboards/OrgPrescription.js
@@ -1,12 +1,47 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {Link} from "react-router-dom"
+import {getPres} from "../CallingApi/patientapi"
 
 function OrgPrescription({history}) {
+    // console.log(history.location.state.userinfo?.UID)
+    const [presData,setPresData] = useState([])
+    const [dataReceived,setDataReceived] = useState(false)
+
+     useEffect(  () => {
+         getPres(history.location.state.userinfo?.UID).
+        then(data => {   console.log(data) 
+             setPresData(data.data)}).
+        catch(err => {console.log(err)})
+        setDataReceived(true);
+     },[]  )
     return (
         <div>
             <h1>View Prescription Here</h1>
             <button onClick = { ( ) => {  history.push("/org/dashboard")   }} >Organisation Dashboard</button> 
-            <button onClick = { ( ) => {  history.push("/org/allDetails", history.location.state)   }} >Organisation all details Dashboard</button> 
+            <button onClick = { ( ) => {  history.push("/org/allDetails", history.location.state)   }} >Organisation all details Dashboard</button>
+            <ol>
+       {presData.map( (data,i) => {
+  
+        const val = data.medDetails.map( (innerData,i) =>  { return <li>
+            
+            <div style = {{color : "green"}}>
+              { i === 0 ?  <div> 
+                  
+                    <h2>Prescribed At :  {data.createdAt} </h2>
+             <h3> 
+              No of Medication :  {data.medDetails.length} 
+            </h3> </div > : "" }
+            <h3>  Medicine Name : {innerData.med_name} Duration : {innerData.duration}  </h3>
+
+              {data.medDetails.length === 1 ? <div>  <br></br>  <br></br> </div> : "" }
+             
+             </div>
+              </li>  }
+            
+              )
+        return val
+      }  )  }  
+      </ol>   
         </div>
     )
 }
